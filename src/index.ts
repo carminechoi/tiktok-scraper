96e1a0fd7683d0f51674d96fd5fa8ed9aeb9f280
Carmine Choi <carmine.choi@gmail.com>
2023-11-14 16:48:35 -0800

    feat: remove inquirer

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,8 @@
 import cron from "node-cron";
-import inquirer from "inquirer";
+import { tiktokScraper } from "./scraper/tiktokScraper";
 
-let cronJob: cron.ScheduledTask | null = null;
-
-const startCronJob = () => {
-	// Your cron job logic goes here
-	cronJob = cron.schedule("* * * * *", () => {
-		console.log("running a task every minute");
-	});
-
-	console.log("Cron job started.");
-};
-
-const stopCronJob = () => {
-	// Add logic to stop or pause the cron job
-
-	if (cronJob) {
-		cronJob.stop();
-		cronJob = null;
-		console.log("Cron job stopped.");
-	} else {
-		console.log("No cron job is currently running.");
-	}
-};
-
-const handleUserInput = async () => {
-	const choices = [
-		"Change Parameters",
-		cronJob ? "Restart Job" : "Start Job",
-		"Stop Job",
-		"Quit",
-	];
-
-	const { action } = await inquirer.prompt([
-		{
-			type: "list",
-			name: "action",
-			message: "Choose an action:",
-			choices: choices,
-		},
-	]);
-
-	switch (action) {
-		case "Change Parameters":
-			console.log("Changing parameters...");
-			break;
-		case "Start Job":
-			startCronJob();
-			break;
-		case "Restart Job":
-			stopCronJob();
-			startCronJob();
-			break;
-		case "Stop Job":
-			stopCronJob();
-			break;
-		case "Quit":
-			// Exit the program
-			console.log("Quitting...");
-			process.exit(0);
-	}
-	handleUserInput();
-};
-
-const init = () => {
-	console.log("Initializing TikTok Scraper...");
-
-	// Listen for User Input
-	handleUserInput();
-};
-
-init();
+// Schedule scraper to run every midnight
+cron.schedule("0 0 * * *", () => {
+	console.log("Running TikTok scraper...");
+	tiktokScraper();
+});

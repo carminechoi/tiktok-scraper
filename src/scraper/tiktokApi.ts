9ffd0b66005f8451533f315f505b60fab71b4021
Carmine Choi <carmine.choi@gmail.com>
2023-11-21 20:32:52 -0800

    feat: add comment api

--- src/scraper/tiktokApi.ts
+++ src/scraper/tiktokApi.ts
@@ -4,14 +4,17 @@ if (!TT_CHAIN_TOKEN || !TTWID) {
 	throw new Error("Missing TikTok API environment variables");
 }
 
-const fetchTikTokVideosByHashtag = async (
+const baseUrl = "https://www.tiktok.com/api";
+
+const fetchTikTokPostsByHashtag = async (
 	hashtag: string = "fashion",
-	offset: number = 0
+	offset: number = 0,
+	searchId: string = ""
 ) => {
 	try {
-		const url = `https://www.tiktok.com/api/search/general/full/?keyword=%23${hashtag}&offset=${offset}`;
-
+		const url = `${baseUrl}/search/general/full/?keyword=%23${hashtag}&offset=${offset}&search_id=${searchId}`;
 		const headers = new Headers({
+			Host: "www.tiktok.com",
 			Cookie: `tt_chain_token=${TT_CHAIN_TOKEN}; ttwid=${TTWID};`,
 		});
 
@@ -28,61 +31,29 @@ const fetchTikTokVideosByHashtag = async (
 	}
 };
 
-const fetchTikTokTrendingVideos = async (count: number = 50) => {
+const fetchTikTokCommentsByPostId = async (
+	id: number,
+	count: number = 20,
+	cursor: number = 0
+) => {
 	try {
-		const url = `https://m.tiktok.com/api/item_list/?count=${count}&id=1&type=5&maxCursor=0&minCursor=0&sourceType=12`;
+		const url = `${baseUrl}/comment/list/?aweme_id=${id}&count=${count}&cursor=${cursor}`;
 		const headers = new Headers({
-			"User-Agent":
-				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
-			Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-			"sec-ch-ua":
-				'"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
-			"sec-ch-ua-mobile": "?0",
-			"sec-ch-ua-platform": '"Windows"',
-			host: "t.tiktok.com",
+			Host: "www.tiktok.com",
 			Cookie: `tt_chain_token=${TT_CHAIN_TOKEN}; ttwid=${TTWID};`,
 		});
 
-		const response = await fetch(url, {
-			method: "get",
-			headers: headers,
-		});
+		const response = await fetch(url, { method: "get", headers: headers });
 
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
 		}
 
-		const data = await response.json();
-		return data.items;
+		return await response.json();
 	} catch (error) {
-		console.error("Error fetching TikTok trending data:", error);
+		console.error("Error fetching TikTok hashtag data:", error);
 		throw error;
 	}
 };
 
-const fetchTiktokVideo = async (videoUrl: string): Promise<string> => {
-	try {
-		const headers = new Headers({
-			Cookie: `tt_chain_token=${TT_CHAIN_TOKEN}; ttwid=${TTWID};`,
-		});
-		const response = await fetch(videoUrl, {
-			method: "get",
-			headers: headers,
-		});
-
-		if (!response.ok) {
-			throw new Error(`HTTP error! Status: ${response.status}`);
-		}
-
-		return await response.text();
-	} catch (error) {
-		console.error("Error fetching TikTok video:", error);
-		throw error;
-	}
-};
-
-export {
-	fetchTikTokVideosByHashtag,
-	fetchTikTokTrendingVideos,
-	fetchTiktokVideo,
-};
+export { fetchTikTokPostsByHashtag, fetchTikTokCommentsByPostId };

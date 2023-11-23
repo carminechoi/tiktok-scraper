c22c0204741a7c898e4301ab524a1415239b26c0
Carmine Choi <carmine.choi@gmail.com>
2023-11-23 01:14:41 -0800

    feat: update index cron schedule

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import "dotenv/config";
 import cron from "node-cron";
 import { tiktokScraper } from "./scraper/tiktokScraper";
 
-// const scheduleScrapper = () => {
-// 	// Schedule scraper to run 1 minute
-// 	cron.schedule("*/10 * * * * *", () => {
-// 		console.log("Running TikTok scraper...");
-// 		tiktokScraper();
-// 	});
-// };
+const scheduleScrapper = () => {
+	// Schedule scraper to run 1 minute
+	cron.schedule("*/5 * * * *", () => {
+		console.log("Running TikTok scraper...");
+		tiktokScraper();
+	});
+};
 
-// scheduleScrapper();
-
-tiktokScraper();
+scheduleScrapper();
